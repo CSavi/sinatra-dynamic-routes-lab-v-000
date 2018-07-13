83c8e49ed4b2f889aa484dbad439e64392b51953
Carrie Savinell <carrielane89@gmail.com>
2018-07-13 14:19:34 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -24,6 +24,6 @@ class App < Sinatra::Base
 
   get '/:operation/:number1/:number2' do
     operation = [:operation]
-    operation(params[:number1].to_i)(params[:number2])
+    operation(params[:number1].to_i)(params[:number2].to_i)
   end
 end
